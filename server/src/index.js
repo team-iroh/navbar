c088e17a3a71a93620179e909ce2a745e8478780
zjayers <zayers.dev@gmail.com>
2020-06-25 14:51:44 -0700

    updated prep for team

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 // Serve up the dist folder from the client at the defined PORT
-app.use(express.static(path.resolve(__dirname, '../../client/dist')));
+app.use(express.static('./', path.resolve(__dirname, '../../client/dist')));
 
 /* This is to allow the app to send the dist html file no matter the params in the request */
 app.get('*', (req, res) => {
